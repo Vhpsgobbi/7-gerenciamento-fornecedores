feafc0664737842862925f3e2155bb48960d3894
VictorSgobbi <victorhugosgobbi@gmail.com>
2026-03-24 00:17:35 -0300

    Criação da entidade Fornecedor

---
+++ src/main/java/com/example/demo/entity/Fornecedor.java
@@ -0,0 +1,56 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Fornecedor")
+@Table(name = "tb_fornecedor")
+public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nm_fantasia")
+    private String nomeFantasia;
+
+    @Column(name = "nr_cnpj")
+    private String cnpj;
+
+    @Column(name = "ds_contato")
+    private String contato;
+
+    public Fornecedor() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
